abde16a4ab3eca3a2d60cba4d77859bd24fbe84b
Oliver <oliver.brouckaert@gmail.com>
2023-03-22 13:08:03 +0200

    Update views.py
    Added docstrings for view functions

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def index(request):
+    """View function in the pages app used to render the index page if the user is logged in or the entry page if not
+
+        :param request: Httprequest object
+
+        :returns: HttpResponse object containing an HTML response of the index HTML if the user is logged in
+        and the entry HTML if the user is not logged in
+
+        :rtype: HttpResponse object containing an HTML response
+    """
     if request.user.is_authenticated:
         return render(request, 'index.html')
     else:
@@ -10,6 +19,15 @@ def index(request):
 
 
 def about(request):
+    """View function in the pages app used to render the about page if the user is logged in or the entry page if not
+
+            :param request: Httprequest object
+
+            :returns: HttpResponse object containing an HTML response of the about.html template if the user is logged in
+            and the entry.html if the user is not logged in
+
+            :rtype: HttpResponse object containing an HTML response
+        """
     if request.user.is_authenticated:
         return render(request, 'about.html')
     else:
@@ -17,6 +35,15 @@ def about(request):
 
 
 def products(request):
+    """View function in the pages app used to render the products.html template if the user is logged in or the entry page if not
+
+                :param request: Httprequest object
+
+                :returns: HttpResponse object containing an HTML response of the products.html template if the user is logged in
+                and the entry.html if the user is not logged in
+
+                :rtype: HttpResponse object containing an HTML response
+            """
     if request.user.is_authenticated:
         return render(request, 'products.html')
     else:
@@ -24,7 +51,17 @@ def products(request):
 
 
 def visit(request):
+    """View function in the pages app used to render the visit.html page if the user is logged in or the entry page if not
+
+                :param request: Httprequest object
+
+                :returns: HttpResponse object containing an HTML response of the visit.html template if the user is logged in
+                and the entry.html if the user is not logged in
+
+                :rtype: HttpResponse object containing an HTML response
+            """
     if request.user.is_authenticated:
         return render(request, 'visit.html')
     else:
         return render(request, 'entry.html')
+
